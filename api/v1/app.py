089c5dadca8976d42c1934b18c9c407f75950517
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-04-07 20:13:14 +0100

    This commit will push the api changes and modification to count method

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,13 +12,13 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
-
 @app.teardown_appcontext
 def teardown_engine(exception):
     """
 
     """
     storage.close()
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -27,6 +27,7 @@ def not_found(error):
     """
     response = {"error": "Not found"}
     return jsonify(response), 404
+
 
 if __name__ == '__main__':
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
